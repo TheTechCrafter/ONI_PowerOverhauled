310629dadfed91a8887ec3217cfb2a68e7b0689f
The Tech Crafter <blankenburgraul@gmail.com>
2025-10-01 21:34:40 +0200

    Sucsessfully removed Vanilla Power Wires

---
+++ PowerOverhauled/Buildings/Wire2Core/Wire2CoreConfig.cs
@@ -0,0 +1,38 @@
+﻿using TUNING;
+using UnityEngine;
+
+namespace PowerOverhauled
+{
+    public class Wire2CoreConfig : IBuildingConfig
+    {
+        public const string ID = "Wire2Core";
+
+        public override BuildingDef CreateBuildingDef()
+        {
+            var def = BuildingTemplates.CreateBuildingDef(
+                ID,
+                1, 1,                                  // Breite, Höhe
+                "electrical_wire_kanim",               // Vanilla-KAnim als Platzhalter
+                10, 10f,                               // hitpoints, construction_time
+                new float[] { 50f },                   // Bau-Masse
+                new string[] { "Copper" },             // Baumaterial
+                800f,                                  // Schmelzpunkt
+                BuildLocationRule.Tile,
+                DECOR.NONE,
+                NOISE_POLLUTION.NONE
+            );
+
+            def.Floodable = false;
+            def.Overheatable = false;
+            def.AudioCategory = "Metal";
+            def.ViewMode = OverlayModes.Power.ID;
+
+            return def;
+        }
+
+        public override void DoPostConfigureComplete(GameObject go)
+        {
+            // Optional: Logik für Stromfluss kann hier eingefügt werden
+        }
+    }
+}
---
+++ PowerOverhauled/ModEntry/PowerOverhauledMod.cs
@@ -0,0 +1,10 @@
+﻿using KMod;
+
+namespace PowerOverhauled
+{
+    // Die UserMod2-Klasse muss vorhanden sein, aber kein OnLoad() mehr
+    public class PowerOverhauledMod : UserMod2
+    {
+        // Mod-Initialisierung läuft über Harmony Patches oder Configs
+    }
+}
